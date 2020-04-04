fa130362cbc3a509583594e044950a7c3b56a708
Christian Lerrahn <christian@drwaryaa.com>
2020-04-05 08:31:40 +1000

    Changed ordering of bootstrap sequence to fix import issues

--- helloneighbour-api/swagger_server/bootstrap.py
+++ helloneighbour-api/swagger_server/bootstrap.py
@@ -6,9 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = connexion.App(__name__, specification_dir='./swagger/')
 app.app.json_encoder = encoder.JSONEncoder
-app.add_api('swagger.yaml', arguments={'title': 'COVID-19 Global Church Hack &#39;Hello Neighbour&#39;'})
 flask_app = app.app
 flask_app.config['SQLALCHEMY_DATABASE_URI'] = config.STORAGE_BACKEND
 flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(flask_app)
 db.create_all()
+app.add_api('swagger.yaml', arguments={'title': 'COVID-19 Global Church Hack &#39;Hello Neighbour&#39;'})
