cec8c815749faf4d7756bde5bc8e580034874401
Christian Lerrahn <christian@drwaryaa.com>
2020-04-04 21:35:05 +1100

    Added link to question to match group

--- helloneighbour-api/swagger_server/storage/match_group.py
+++ helloneighbour-api/swagger_server/storage/match_group.py
@@ -10,6 +10,11 @@ class MatchGroup(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     pub_id = db.Column(db.Text, default=uuid4, unique=True)
     created = db.Column(db.DateTime, default=datetime.utcnow)
+    question_id = db.Column(
+        db.Integer,
+        db.ForeignKey('question.id'),
+        nullable=False
+    )
 
     users = db.relationship(
         "User",
